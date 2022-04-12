b706b51fbb2a83e36267777f22fb02f17c078f4a
Kevin Hernandez <kevin.hernandez288@gmail.com>
2022-04-12 15:56:19 -0700

    Linked Pages with Testing Javascript Content

---
+++ assets/scripts/chart.js
@@ -0,0 +1,58 @@
+// USE THESE FOR INTRA-DAY INFORMATION
+var apiKey = "2JYN2GFONTCPQSJM";
+var ticker = "IBM"; 
+var time = "5min"; // for intra-day intervals
+var apiLink = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol="+ticker+"&interval="+time+"&apikey="+apiKey;
+var apiLinkDay = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol="+ticker+"&apikey="+apiKey;
+
+const ctx = document.getElementById('myChart').getContext('2d');
+const myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: ticker,
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
+
+var completeData;
+
+function getAPI3(inputLink) {
+    $.ajax({
+        url: inputLink,
+        method: "GET"
+    }).then(function (data) {
+        // completeData = data;
+        console.log(data["Meta Data"]);
+        completeData = data["Meta Data"];
+        return data;
+    });
+}
+
+getAPI3(apiLinkDay);
