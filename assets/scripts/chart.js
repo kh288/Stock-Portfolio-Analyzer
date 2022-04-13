9bedc85ad51a0ec570ce7334e451a641c6ba08dd
Kevin Hernandez <kevin.hernandez288@gmail.com>
2022-04-12 18:18:00 -0700

    Add solution for converting API time to chart Time

--- assets/scripts/chart.js
+++ assets/scripts/chart.js
@@ -8,24 +8,54 @@ var apiLinkDay = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&s
 // var luxTest = luxon
 // console.log(luxon.DateTime)
 
-const startingdate = luxon.DateTime.fromRFC2822('01 Aug 2021 00:00 GMT')
-const startingdate2 = luxon.DateTime.fromRFC2822('02 Aug 2021 00:00 GMT')
+// SAMPLE DATA
+// "Time Series (Daily)": {
+//     "2022-04-12": {
+//     "1. open": "126.4200",
+//     "2. high": "127.3400",
+//     "3. low": "125.5835",
+//     "4. close": "125.9800",
+//     "5. volume": "2670077"
+//     },
+//     "2022-04-11": {
+//     "1. open": "127.9500",
+//     "2. high": "128.1750",
+//     "3. low": "126.1800",
+//     "4. close": "126.3700",
+//     "5. volume": "3202545"
+//     },
+//     "2022-04-08": {
+//     "1. open": "128.0100",
+//     "2. high": "128.7800",
+//     "3. low": "127.2700",
+//     "4. close": "127.7300",
+//     "5. volume": "3143309"
+//     },
+//     "2022-04-07": {
+//     "1. open": "128.8700",
+//     "2. high": "129.2499",
+//     "3. low": "126.7300",
+//     "4. close": "128.5500",
+//     "5. volume": "3538317"
+//     }
 
-console.log(startingdate)
+// We use .fromSQL to convert into the proper INPUT time we're getting from the API
+const testTime = luxon.DateTime.fromSQL('2021-11-18');
+const testTime2 = luxon.DateTime.fromSQL('2021-11-19');
 
 
 const data =  {
     datasets: [{
         data: [
         {
-            x: startingdate.valueOf(),
+            x: testTime.valueOf(),
             o: 1,
             h: 1.50,
             l: 0.75,
             c: 1.25
         },
         {
-            x: startingdate2.valueOf(),
+            x: testTime2.valueOf(),
             o: 1.25,
             h: 2,
             l: 1.00,
