4bb42c48d9c8431bdc4e11f67fa5e6976b1636b3
John Netzel <johnrnetzel@gmail.com>
2022-04-12 18:46:15 -0800

    Add delete position button to positions

--- assets/scripts/portfolio.js
+++ assets/scripts/portfolio.js
@@ -4,6 +4,12 @@ const addPositionEl = $("#button-add-position");
 const saveButtonEl = $("#button-save-portfolio");
 const deleteButtonEl = $("#button-delete-portfolio");
 
+portfolioListEl.on("click", ".delete-position", function()
+{
+    alert(this);
+    $(this).parent().remove();
+});
+
 addPositionEl.on("click", function()
 {
     const positionEl = $("<div>");
@@ -16,14 +22,17 @@ addPositionEl.on("click", function()
     priceEl.text("Current Price: $165.75 (-4.34 -2.55%)");
     const positionPriceEl = $("<p>");
     positionPriceEl.text("Position Value: $8,375.00 (-$217.00)");
+    const deleteButtonEl = $("<button class='delete-position' type='button'>");
+    deleteButtonEl.text("Delete Position");
     
     tickerEl.appendTo(positionEl);
     amountEl.appendTo(positionEl);
     priceEl.appendTo(positionEl);
     positionPriceEl.appendTo(positionEl);
+    deleteButtonEl.appendTo(positionEl);
 
     positionEl.appendTo(portfolioListEl);
-})
+});
 
 saveButtonEl.on("click", function()
 {
