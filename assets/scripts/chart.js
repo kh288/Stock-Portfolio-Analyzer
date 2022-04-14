64ba80dc3c1d97eced71e44da51bda99ebc3b000
Kevin Hernandez <kevin.hernandez288@gmail.com>
2022-04-14 13:24:49 -0700

    Cleaned up old comments

--- assets/scripts/chart.js
+++ assets/scripts/chart.js
@@ -45,26 +45,15 @@ async function getAPI(inputLink) {
         method: "GET"
     }).then(function (output) {
         // chartData = output["Time Series (Daily)"];
-        // console.log(output);
-        // chartData = output;
-        // console.log(dataTrimmed);
-        // apiData = output["Time Series (Daily)"];
         apiData = output["Weekly Time Series"];
 
         // dataParsed = parseLineData(output["Time Series (Daily)"]);
         dataParsed = parseLineData(output/* ["Weekly Time Series"] */);
-
-        // console.log(Object.keys(output["Monthly Time Series"]))
-    
-
-        // parseLineData(dataSets)
         
         const chartData = {
             labels: dataParsed[1],
             datasets: dataParsed[0]
         };
-
-        // console.log(data);
 
         const config = {
             type: 'line',
@@ -78,112 +67,6 @@ async function getAPI(inputLink) {
 }
 
 //getAPI(apiLinkDay);
-
-
-// function parseCandleData(apiInput) {
-//     // This is the main for loop to create our candle sticks from the API information
-//     for (i = 0; i < Object.keys(apiInput).length; i++) {
-//         // This creates a new object for each candle for the dataset to live
-//         data.datasets[0].data[i] = {
-//             x: luxon.DateTime.fromSQL(Object.keys(apiInput)[i]).valueOf(),
-//             o: apiInput[Object.keys(apiInput)[i]]["1. open"],
-//             h: apiInput[Object.keys(apiInput)[i]]["2. high"],
-//             l: apiInput[Object.keys(apiInput)[i]]["3. low"],
-//             c: apiInput[Object.keys(apiInput)[i]]["4. close"]
-//         }
-//     }
-//     console.log(data);
-// }
-
-// function to fetch the API key and execute the chart as well since we're waiting for the data
-// function getAPI(inputLink) {
-//     // We need to fetch the data since it takes some time to retreive
-//     $.ajax({
-//         url: inputLink,
-//         method: "GET"
-//     }).then(function (output) {
-//         // Get data from API and store it into our data variable
-//         console.log(output);
-//         var data = createChartData(output["Weekly Adjusted Time Series"]);
-//         console.log(data);
-//         // This config object is used in the chart, we're also putting chart in it
-//         const config = {
-//             type: 'candlestick',
-//             data
-//         };
-//         // store data from API into a global variable
-//         chartData = data;
-//         // Create a chart. Use the data we told it to use from earlier
-//         const myChart = new Chart(document.getElementById('myChart'),config);
-
-//         // createChartData()
-//         return;
-//     });
-// }
-
-// function createChartData(apiInput) {
-//     var data =  {
-//         datasets: [{
-//             label: ticker,
-//             data: []
-//         }]
-//     };
-
-// function createLineChart() {
-//     const labels = Utils.months({count: 7});
-
-//     const data = {
-//       labels: labels,
-//       datasets: [{
-//         label: 'My First Dataset',
-//         data: [65, 59, 80, 81, 56, 55, 40],
-//         fill: false,
-//         borderColor: 'rgb(75, 192, 192)',
-//         tension: 0.1
-//       }]
-//     };
-
-//     const config = {
-//         type: 'line',
-//         data: data
-//     };
-//     const lineChart = new Chart(document.getElementById('line-chart'),config);
-//     return lineChart;
-// }
-
-// run the function with our api link arguement
-
-// sum of all closing dates for stock and consolidate it into chart
-
-// const data = {
-//   labels: ["Jan", "Feb", "March", "April", "May", "June", "July"],
-//   datasets: [{
-//     label: 'AAPL',
-//     data: [65, 59, 80, 81, 56, 55, 40],
-//     fill: false,
-//     borderColor: 'rgb(75, 192, 192)',
-//     tension: 0.25
-//   }, {
-//     label: 'MFST',
-//     data: [45, 39, 40, 51, 26, 45, 30],
-//     fill: false,
-//     borderColor: '#336533',
-//     tension: 0.25
-//   }, {
-//     label: 'TOTAL',
-//     data: [100, 139, 140, 151, 126, 145, 130],
-//     fill: false,
-//     borderColor: 'rgb(75, 192, 192)',
-//     tension: 0.25
-//   }
-
-// ]
-// };
-// const config = {
-//     type: 'line',
-//     data: data
-// };
-// const lineChart = new Chart(document.getElementById('line-chart'),config);
 
 // grabs local storage data and displays names in the aside bar 
 //insertPortName();
@@ -210,6 +93,7 @@ function clear(){
     //card.empty()
 
 }
+
 // add click event listener to portfolio and stats location
 var ticker;
 card.on('click',function(event){
@@ -226,6 +110,7 @@ card.on('click',function(event){
 
 
 })
+
 //display current name of portfolio and value
 // function portfolioValue(portfolio,currentPrice){
 //     var value = 0;
@@ -239,3 +124,4 @@ card.on('click',function(event){
 //     var tvDisplay = $('<div>')
 //     tvDisplay.text(portfolio.name + " " +'value: '+ '$' + value)  } 
 //     tvDisplay.appendTo(card);
+
